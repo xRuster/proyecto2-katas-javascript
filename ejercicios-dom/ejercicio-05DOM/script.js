c03e817a1472cf2ac0f0f7fb0371d2cef01c97b9
Juan L <rusterthepower@gmail.com>
2026-01-11 18:47:24 +0100

    11_01_2026

--- ejercicios-dom/ejercicio-05DOM/script.js
+++ ejercicios-dom/ejercicio-05DOM/script.js
@@ -8,6 +8,7 @@ const albums = [
 
 
 
+
 const album = document.createElement("ul")
 album.className = "lista-album"
 document.body.appendChild(album)
@@ -15,6 +16,11 @@ document.body.appendChild(album)
 const h1 = document.createElement("h1")
 h1.textContent = "MEJORES ALBUMES"
 album.appendChild(h1)
+
+const imgBg = document.createElement("img")
+imgBg.className = "img-bg"
+imgBg.src ="../ejercicio-05DOM/media/gettyimages-523347032-612x612.jpg"
+document.body.appendChild(imgBg)
 
 const div = document.createElement("div")
 album.appendChild(div)
@@ -25,14 +31,40 @@ const changeBgColor = (event) => {
 
 
 for (let cancion of albums){
-    const li = document.createElement("li")
+    
+    let li = document.createElement("li")
     li.className = "listaElemento"
     li.textContent = cancion
+    li.dataset.nombre = cancion
     
-    li.addEventListener("click",changeBgColor)
+    //li.addEventListener("click",changeBgColor)
 
     album.appendChild(li)
 
+
 }
 
+
+
+
+const changeLetterColor = (event) => {
+    event.target.classList.toggle("colorClassTitle")
+}
+
+h1.addEventListener("click",changeLetterColor)
+
+
+
+const mostrarImgDisco = (event) => {
+  event.target.classList.toggle("img-desplegar")
+  if(event.target.classList.contains("img-desplegar")){
+    event.target.textContent = ""
+  }else{
+    event.target.textContent = event.target.dataset.nombre
+    
+  }
+}
+
+
+document.querySelector("li").addEventListener("click",mostrarImgDisco)
 
